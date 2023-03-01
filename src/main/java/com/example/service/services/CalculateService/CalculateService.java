d55faf952d77249140f9cc307413e0e56420d3ef
preethi <preethianbhu23@gmail.com>
2023-03-01 17:44:15 +0530

    -Unit Test cases -Changes in Functions for equal to case

--- src/main/java/com/example/service/services/CalculateService/CalculateService.java
+++ src/main/java/com/example/service/services/CalculateService/CalculateService.java
@@ -5,23 +5,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculateService {
 
-   /***
-    * Do calculation
-    * @param n1 : Number in Double format
-    * @param n2 : Number in Double format
-    * @return Number in Double format after calculation
-    */
-   public Double Calculate(Double  n1 , Double n2){
-      Double result ;
+    /***
+     * Do calculation
+     * @param n1 : Number in Double format
+     * @param n2 : Number in Double format
+     * @return Number in Double format after calculation
+     */
+    public Double Calculate(Double n1, Double n2) {
+        Double result;
+        if (n1 < n2) {
+            result = n2 * 30000 / 3.1415;
+        } else if (n2 < n1) {
+            result = n1 * 30000 / 3.1415;
+        } else {
+            result = 0.0;
+        }
 
-         if( n1 < n2 ){
-           result =n2*30000/3.1415;
-         }
-         else{
-          result=n1*30000/3.1415;
-         }
-                    
-         return result;
-   }
+        return result;
+    }
 
 }
---
+++ src/test/java/com/example/service/CalculateService/CalculateServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.service.CalculateService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.service.services.CalculateService.CalculateService;
+
+@SpringBootTest
+class CalculateServiceTest {
+
+
+    private CalculateService calculateService = new CalculateService();
+
+    /**
+     * Test -case for calcaulte functions
+     */
+    @Test
+    void CalculateN1GreaterThanN2() {
+        Double expectedResult = 9.431163456947318E7;
+        Double n1 = 9876.0;
+        Double n2 = 123.0;
+        Double result = calculateService.Calculate(n1, n2);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+
+    @Test
+    void CalculateN2GreaterThanN1() {
+        Double expectedResult = 114594.9387235397;
+        Double n1 = 9.0;
+        Double n2 = 12.0;
+        Double result = calculateService.Calculate(n1, n2);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+
+    @Test
+    void CalculateN1EqualToN2() {
+        Double expectedResult = 0.0;
+        Double n1 = 1.0;
+        Double n2 = 1.0;
+        Double result = calculateService.Calculate(n1, n2);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+
+    @Test
+    void CalculateFalseCase() {
+        Double expectedResult = 9.31163456947318E7;
+        Double n1 = 12.0;
+        Double n2 = 9.0;
+        Double result = calculateService.Calculate(n1, n2);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+}
