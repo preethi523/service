4f8aced22f877eff6af46bc5f2b28b64ab5d9b94
preethi <preethianbhu23@gmail.com>
2023-03-01 14:46:58 +0530

    -Added Calculate function

--- src/main/java/com/example/service/controller/MainController.java
+++ src/main/java/com/example/service/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.example.service.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.services.CalculateService.CalculateService;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/calculate")
+    @Autowired
+    private CalculateService calculateValue;
+
+    @GetMapping("/welcome")
     public String welcome(){
         return "welcome to spring Boot App";
+    }
+
+    @GetMapping("/calculate/{n1}/{n2}")
+    public Double calculate(@PathVariable Double n1,@PathVariable Double n2){
+       return  this.calculateValue.Calculate(n1, n2);
     }
     
 }
---
+++ src/main/java/com/example/service/services/CalculateService/CalculateService.java
@@ -0,0 +1,27 @@
+package com.example.service.services.CalculateService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculateService {
+
+   /***
+    * Do calculation
+    * @param n1 : Number in Double format
+    * @param n2 : Number in Double format
+    * @return Number in Double format after calculation
+    */
+   public Double Calculate(Double  n1 , Double n2){
+      Double result ;
+
+         if( n1 < n2 ){
+           result =n2*30000/3.1415;
+         }
+         else{
+          result=n1*30000/3.1415;
+         }
+                    
+         return result;
+   }
+
+}
